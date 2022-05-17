ef0a134e08c3406d72a3f44eb8b2c98e4ff95c8a
OheJaeWook <worl5@korea.ac.kr>
2022-05-17 22:14:44 +0900

    2022.05.17_study_login

--- study_JS.js
+++ study_JS.js
@@ -349,3 +349,71 @@
 // }
 
 // loginForm.addEventListener("submit", submitLogIn);
+
+const loginForm = document.querySelector("#login-form");
+// const loginInput = loginForm.querySelector("#login-form input");
+// const greeting = document.querySelector("#greeting");
+
+// const HIDDEN_CLASSNAME = "hidden";
+// const USERNAME_KEY = "username";
+
+// function paintGreetings(username) {
+//   greeting.innerText = `Hello ${username}`;
+//   greeting.classList.remove(HIDDEN_CLASSNAME);
+// }
+
+// // submitLogIn(JS gives you info which you need with first argu);
+// function submitLogIn(event) {
+//   event.preventDefault();
+//   const username = loginInput.value;
+//   loginForm.classList.add(HIDDEN_CLASSNAME);
+//   paintGreetings(username);
+
+//   localStorage.setItem(USERNAME_KEY, username);
+// }
+
+// const savedUsernam = localStorage.getItem(USERNAME_KEY);
+// if (savedUsernam === null) {
+//   // show form
+//   loginForm.classList.remove(HIDDEN_CLASSNAME);
+//   loginForm.addEventListener("submit", submitLogIn);
+// } else {
+//   // show greeting
+//   paintGreetings(savedUsernam);
+// }
+
+const loginForm = document.querySelector("#login-form");
+// const loginInput = loginForm.querySelector("#login-form input");
+// const greeting = document.querySelector("#greeting");
+
+// const HIDDEN_CLASSNAME = "hidden";
+// const USERNAME_KEY = "username";
+
+// function paintGreetings() {
+//   const username = localStorage.getItem(USERNAME_KEY);
+//   greeting.classList.remove(HIDDEN_CLASSNAME);
+
+//   greeting.innerText = `Hello ${username}`;
+// }
+
+// // submitLogIn(JS gives you info which you need with first argu);
+// function submitLogIn(event) {
+//   event.preventDefault();
+//   /////////////////////////
+//   const username = loginInput.value;
+//   loginForm.classList.add(HIDDEN_CLASSNAME);
+//   localStorage.setItem(USERNAME_KEY, username);
+//   /////////////////////////
+//   paintGreetings();
+// }
+
+// const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+// if (savedUsername === null) {
+//   // show form
+//   loginForm.classList.remove(HIDDEN_CLASSNAME);
+//   loginForm.addEventListener("submit", submitLogIn);
+// } else {
+//   // show greeting
+//   paintGreetings();
+// }
